956dc6fdaed4c04ed0092720fcdab2f9487639f6
key.cheng <key.cheng@104.com.tw>
2025-08-26 09:37:39 +0800

    [feat][MIS-001] 新增 GitHub Pages 部署工作流程及 404 頁面

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+  ssr: false,
   modules: ['@nuxt/eslint', '@nuxt/image', '@unocss/nuxt'],
-  css: ['~/assets/styles/base.scss']
+  css: ['~/assets/styles/base.scss'],
+  nitro: {
+    prerender: {
+      routes: ['/']
+    }
+  },
+  app: {
+    baseURL: '/aidetect/'
+  }
 })
